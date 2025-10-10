d869f4ce329dcf65f903e7212ad07b1044aebbd8
Nahom <yacob.nahom@outlook.com>
2025-10-10 10:53:07 +0200

    Roomview

---
+++ player.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+@export var speed := 200
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	position += (direction * speed) * delta
+	move_and_slide()
