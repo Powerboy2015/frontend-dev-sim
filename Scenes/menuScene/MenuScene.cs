c8138ac26936721f69085e03a010201ab059fcf7
Spoekle <thiangraber@gmail.com>
2025-10-16 09:32:21 +0200

    moved headers to be in the same position

--- Scenes/menuScene/MenuScene.cs
+++ Scenes/menuScene/MenuScene.cs
@@ -8,27 +8,27 @@ public partial class MenuScene : Control
 		// Hoppa pak die singleton erbij
 		var config = GetNode<ConfigManager>("/root/ConfigManager");
 		
-		GetNode<Button>("AspectRatioContainer/VBoxContainer/VBoxContainer/StartButton").Pressed += () =>
+		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/StartButton").Pressed += () =>
 		{
 			GetTree().ChangeSceneToFile("res://scenes/gameScene/gameScene.tscn");
 		};
 		
-		GetNode<Button>("AspectRatioContainer/VBoxContainer/VBoxContainer/SettingsButton").Pressed += () =>
+		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/SettingsButton").Pressed += () =>
 		{
 			GetTree().ChangeSceneToFile("res://scenes/settingsScene/settingsScene.tscn");
 		};
 		
-		GetNode<Button>("AspectRatioContainer/VBoxContainer/VBoxContainer/HowToPlayButton").Pressed += () =>
+		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/HowToPlayButton").Pressed += () =>
 		{
 			GetTree().ChangeSceneToFile("res://scenes/howToPlayScene/howToPlayScene.tscn");
 		};
 		
-		GetNode<Button>("AspectRatioContainer/VBoxContainer/VBoxContainer/CreditsButton").Pressed += () =>
+		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/CreditsButton").Pressed += () =>
 		{
 			GetTree().ChangeSceneToFile("res://scenes/howToPlayScene/howToPlayScene.tscn");
 		};
 		
-		GetNode<Button>("AspectRatioContainer/VBoxContainer/VBoxContainer/QuitButton").Pressed += () =>
+		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/QuitButton").Pressed += () =>
 		{
 			GetTree().Quit();
 		};
