1e0f88ae71ec14a70e5d950569336fa5b17e7a26
lars <larskonermann@gmail.com>
2025-10-17 09:38:14 +0200

    extra items

---
+++ Scenes/BuyItems/Plant1/Plant1.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Plant1 : Control
+{
+	private Button BuyButton;
+	
+	public override void _Ready()
+	{
+		BuyButton = GetNode<Button>("Panel/BuyButton");
+		BuyButton.Pressed += HandleButtonPressed;
+	}
+	
+	private void HandleButtonPressed()
+	{
+		Wallet.Instance.RemoveCoins(100);
+	}
+}
