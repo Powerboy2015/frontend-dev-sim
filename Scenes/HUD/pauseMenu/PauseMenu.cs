9c70d1b08065d2c51783361589f2879fdd75dd04
Spoekle <thiangraber@gmail.com>
2025-11-04 13:57:20 +0100

    added continue button and game pausing

--- Scenes/HUD/pauseMenu/PauseMenu.cs
+++ Scenes/HUD/pauseMenu/PauseMenu.cs
@@ -5,12 +5,20 @@ public partial class PauseMenu : CanvasLayer
 {
 	public override void _Ready()
 	{
+		GetNode<Button>("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/Continue").Pressed += () =>
+		{
+			AudioHandler.Instance.PlaySFX(SFXType.Click);
+			var menu = GetNode<CanvasLayer>("./");
+			menu.Hide();
+			GetTree().Paused = false;
+		};
 		
-	}
-	
-	public override void _Process(double delta)
-	{
-		
+		GetNode<Button>("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/Menu").Pressed += () =>
+		{
+			AudioHandler.Instance.PlaySFX(SFXType.Click);
+			GetTree().Paused = false;
+			GetTree().ChangeSceneToFile("res://scenes/menuScene/menuScene.tscn");
+		};
 	}
 	
 }
