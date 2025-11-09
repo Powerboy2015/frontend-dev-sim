9bab661c3f7e95ee271e209b6745a714b7c9d497
Zico <150016766+Powerboy2015@users.noreply.github.com>
2025-11-09 14:35:54 +0100

    Outside world (#16)
    * 'update'
    
    * wallet import? & start on general player character and world
    
    * outside world camera and boundaries
    
    - added an outside world scene that allows the user to walk around
    - added world boundaries keeps user inside
    - added camera boundaries that limits camera inside the map
    - limited player space until further features are developed
    
    * outside world camera and boundaries
    
    - added an outside world scene that allows the user to walk around
    - added world boundaries keeps user inside
    - added camera boundaries that limits camera inside the map
    - limited player space until further features are developed
    - updated tile set up in order set up future teleport room

--- Scenes/menuScene/MenuScene.cs
+++ Scenes/menuScene/MenuScene.cs
@@ -7,31 +7,31 @@ public partial class MenuScene : Control
 	{
 		// Hoppa pak die singleton erbij
 		var config = GetNode<ConfigManager>("/root/ConfigManager");
-		
+
 		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/StartButton").Pressed += () =>
 		{
 			AudioHandler.Instance.PlaySFX(SFXType.Click);
 			GetTree().ChangeSceneToFile("res://scenes/storeScene/Shop.tscn");
 		};
-		
+
 		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/SettingsButton").Pressed += () =>
 		{
 			AudioHandler.Instance.PlaySFX(SFXType.Click);
 			GetTree().ChangeSceneToFile("res://scenes/menuScene/settingsScene/settingsScene.tscn");
 		};
-		
+
 		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/HowToPlayButton").Pressed += () =>
 		{
 			AudioHandler.Instance.PlaySFX(SFXType.Click);
 			GetTree().ChangeSceneToFile("res://scenes/menuScene/howToPlayScene/howToPlayScene.tscn");
 		};
-		
+
 		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/CreditsButton").Pressed += () =>
 		{
 			AudioHandler.Instance.PlaySFX(SFXType.Click);
 			GetTree().ChangeSceneToFile("res://scenes/menuScene/creditsScene/creditsScene.tscn");
 		};
-		
+
 		GetNode<Button>("MarginContainer/VBoxContainer/VBoxContainer/QuitButton").Pressed += () =>
 		{
 			AudioHandler.Instance.PlaySFX(SFXType.Click);
